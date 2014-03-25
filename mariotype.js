08349cde22cb6ee65229d8bac7b55fa1b1c5a7c5
Robert L. Read <read.robert@gmail.com>
2014-03-24 22:25:17 -0400

    improving example

--- mariotype.js
+++ mariotype.js
@@ -20,6 +20,12 @@ disableVeteranOwnedBusiness.name = "DVOB";
 womanOwnedBusiness = new Compliance();
 womanOwnedBusiness.name = "WOB";
 
+GREEN = new Compliance();
+GREEN.name = "Green";
+
+BPA = new Compliance();
+BPA.name = "BPA";
+
 
 CatalogItem = function CatalogItem() {
     // We will allow the description to be versioned and to 
@@ -140,7 +146,7 @@ C2ResearchAPI.prototype = new C2ResearchAPI;
 
 var example_carts = new Array();
 var first = new ListCart();
-first.name = "Rob's Magical Mystery Cart";
+first.name = "Rob's Mystery Cart";
 first.items = new Array();
 first.items[0] = new OrderItem();
 first.items[0].units = 50;
@@ -166,7 +172,56 @@ first.items[1].ci.unit_price = 6.35;
 first.items[1].ci.vendor = "Office Surplus";
 first.items[1].ci.compliances[0] = womanOwnedBusiness;
 
+
+var second = new ListCart();
+second.name = "Adam's Example Cart";
+second.items = new Array();
+second.items[0] = new OrderItem();
+second.items[0].units = 25;
+second.items[0].ci = new CatalogItem();
+second.items[0].ci.name = "binder";
+second.items[0].ci.description = "ROUND RING VIEW BINDER WITH INTERIOR POC";
+second.items[0].ci.detail = "7510015194381, Round Ring View Binder with Interior Pockets, White, Capacity 1 1/2 in 30% Recycled content, 30% Post Consumer Recycled Content,JWOD/AbilityOne";
+second.items[0].ci.vehicle = "GSA Advantage";
+second.items[0].ci.SKU = "7510-01-519-4381";
+second.items[0].ci.unit_price = 2.46;
+second.items[0].ci.vendor = "DOCUMENT IMAGING DIMENSIONS INC.";
+second.items[0].ci.compliances[0] = BPA;
+second.items[0].ci.compliances[1] = GREEN;
+
+second.items[1] = new OrderItem();
+second.items[1].units = 5;
+second.items[1].ci = new CatalogItem();
+second.items[1].ci.name = "roller pen";
+second.items[1].ci.description = "PEN,ROLLER,GELINK,G-2,X-FINE";
+second.items[1].ci.detail = "OD ITEM# 790921 Pilot(R) G-2(TM) Retractable Gel Ink Rollerball Pens, 05 mm, Extra Fine Point, Clear Barrel, Blue Ink, Pack Of 12PIL31003Pilot(R) G-2(TM) Retractable Gel Ink Rollerball Pens, 05 mm, Extra Fine Point, Clear Barrel, Blue Ink, Pack Of 12 \
+\
+Additional Description\
+Pilot G2 Retractable Rollerball Pen - Fine Pen Point Type - 0.5 mm Pen Point Size - Blue Ink - Translucent Barrel - 12 / Dozen";
+second.items[1].ci.vehicle = "GSA Advantage";
+second.items[1].ci.SKU = "072838310033";
+second.items[1].ci.unit_price = 10.29;
+second.items[1].ci.vendor = "OFFICE DEPOT";
+second.items[1].ci.compliances[0] = BPA;
+
+
+second.items[2] = new OrderItem();
+second.items[2].units = 3;
+second.items[2].ci = new CatalogItem();
+second.items[2].ci.name = "PAPER,LEDGER,11X8.5";
+second.items[2].ci.description = "PEN,ROLLER,GELINK,G-2,X-FINE";
+second.items[2].ci.detail = "Plain Ledger Paper, 11\"x8-1/2\", 100/BX, White White ledger paper is punched for three 3/16\" x 7/16\" rectangular posts. Ledger sheets offer 8-1/4\" outside post spacing. Paper is designed for use with Wilson Jones 0395-11, 0396-11 and 0399-00. Acco/Wilson Jones,Books,Minute,Records,Meeting Notes,Journals,Refills,ACCO Brands,Ledger Paper\
+\
+Additional Description\
+Wilson Jones Ledger Paper Refill Sheet - Letter 8.50\" x 11\" - 100 / Box - White Paper";
+second.items[2].ci.vehicle = "GSA Advantage";
+second.items[2].ci.SKU = "072838310033";
+second.items[2].ci.unit_price = 32.67;
+second.items[2].ci.vendor = "METRO OFFICE PRODUCTS";
+second.items[2].ci.compliances[0] = BPA;
+
 example_carts['1'] = first;
+example_carts['2'] = second;
 
 mock_C2ResearchAPI = new C2ResearchAPI;
 
