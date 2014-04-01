88278691ac40fb3a6622146db86a24879a9be5f5
Robert L. Read <read.robert@gmail.com>
2014-03-31 22:06:51 -0400

    Parsing of result, much better still a ways to go.

--- mailreader.js
+++ mailreader.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var Imap = require('imap'),
-    inspect = require('util').inspect;
+inspect = require('util').inspect;
 
 var c2render = require('./c2render');
 
@@ -8,6 +8,8 @@ var c2render = require('./c2render');
 fs = require('fs');
 var http = require('http');
 var nodemailer = require("nodemailer");
+
+var simulatedMapOfUserNameToEmail = { "ROBERTLREAD" : "read.robert@gmail.com" };
 
 var dynoCartSender;    // This is a quick and dirty way to keep the user
 
@@ -25,25 +27,25 @@ function instantiateGmailTransport(username, password) {
 var dynoCartXport = instantiateGmailTransport(
     'communicart.sender@gmail.com',
     process.env.COMMUNICART_DOT_SENDER
-    );
+);
 
 function sendFrDynoCart(from, recipients, subject, message) {
     var fromString = from + ' <' + dynoCartSender +'>';
     console.log('from is "' + fromString + '"');
     dynoCartXport.sendMail(
         {
-        from: fromString,
-        to: recipients,
-        subject: subject,
-        text: "you need an html capable email client",
-        html: message
+            from: fromString,
+            to: recipients,
+            subject: subject,
+            text: "you need an html capable email client",
+            html: message
         },
         function(error, response){
             if (error) {
                 console.log(error);
             } else {
                 console.log("Message sent(from " + from + "): " +
-                        response.message);
+                            response.message);
             }
         });
 }
@@ -55,15 +57,15 @@ var GSA_SCRAPE_URL = 'http://gsa-advantage-scraper/cgi-bin/gsa-adv-cart.py';
 
 // taken from StackOverflow: http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
 if (!String.format) {
-  String.format = function(format) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    return format.replace(/{(\d+)}/g, function(match, number) { 
-      return typeof args[number] != 'undefined'
-        ? args[number] 
-        : match
-      ;
-    });
-  };
+    String.format = function(format) {
+	var args = Array.prototype.slice.call(arguments, 1);
+	return format.replace(/{(\d+)}/g, function(match, number) { 
+	    return typeof args[number] != 'undefined'
+		? args[number] 
+		: match
+	    ;
+	});
+    };
 }
 
 
@@ -72,41 +74,109 @@ var GSA_PASSWORD = process.env.GSA_PASSWORD;
 var GSA_USERNAME = process.env.GSA_USERNAME;
 
 var imap = new Imap({
-  user: 'communicart.sender@gmail.com',
-  password: process.env.COMMUNICART_DOT_SENDER,
-  host: 'imap.gmail.com',
-  port: 993,
-  tls: true
+    user: 'communicart.sender@gmail.com',
+    password: process.env.COMMUNICART_DOT_SENDER,
+    host: 'imap.gmail.com',
+    port: 993,
+    tls: true
 });
 
 function openInbox(cb) {
-  imap.openBox('INBOX', true, cb);
+    imap.openBox('INBOX', true, cb);
 }
 
-function parseCartIdFromGSAAdvantage(str) {
-    var reg = /GSA Advantage! cart # (\d+)/gm;
+// Currently these are operating on the COMPLETE 
+// email message.  This is very inefficient, and 
+// can be made significantly better which the time comes.
+
+EmailAnalysis = function EmailAnalysis() {
+    this.cartNumber = "";
+    this.category = "";
+    this.attention = "";
+    this.fromAddress = "";
+    this.gsaUserName = "";
+}
+
+EmailAnalysis.prototype = new EmailAnalysis;
+
+function parseCompleteEmail(str,reg) {
     var myArray = reg.exec(str);
     if (myArray) {
-    var arrayLength = myArray.length;
-    for (var i = 0; i < arrayLength; i++) {
-        console.log("number "+myArray[i]);
-    }
-	var cartNumber = myArray[1];
-	return cartNumber;
+//	var arrayLength = myArray.length;
+//	for (var i = 0; i < arrayLength; i++) {
+//            console.log("number "+myArray[i]);
+//	}
+	var result = myArray[1];
+	return result;
     }
     return null;
 }
 
+function parseCartIdFromGSAAdvantage(str) {
+    var reg = /GSA Advantage! cart # (\d+)/gm;
+    return parseCompleteEmail(str,reg);
+}
+
 function parseAtnFromGSAAdvantage(str) {
     var reg = /\[Atn: (\S+@\S+\.\S+)\]/gm;
+    return parseCompleteEmail(str,reg);
+}
+
+function parseFromEmail(str) {
+    var reg = /From: .* <(\S+@\S+\.\S+)>/gm;
+    return parseCompleteEmail(str,reg);
+}
+
+function parseGsaUsername(str) {
+    var reg = /To: (\S+) <communicart.sender@gmail.com>/gm;
+    return parseCompleteEmail(str,reg);
+}
+
+function parseDate(str) {
+    var reg = /Date: (.*)/gm;
+    return parseCompleteEmail(str,reg);
+}
+	    
+function consolePrint(analysis) {
+    console.log("analysis.category "+analysis.category);
+    console.log("analysis.cartNumber "+analysis.cartNumber);
+    console.log("analysis.fromAddress "+analysis.fromAddress);
+    console.log("analysis.gsaUsername "+analysis.gsaUsername);
+    console.log("analysis.date "+analysis.date);
+}
+function analyze_category(str) {
+    var analysis = new EmailAnalysis();
+    var reg = /Subject: GSA Advantage! cart # (\d+)/gm;
     var myArray = reg.exec(str);
     if (myArray) {
-    var arrayLength = myArray.length;
-    for (var i = 0; i < arrayLength; i++) {
-        console.log("address "+myArray[i]);
-    }
+	var arrayLength = myArray.length;
+	for (var i = 0; i < arrayLength; i++) {
+//            console.log("address "+myArray[i]);
+	}
 	var cartNumber = myArray[1];
-	return cartNumber;
+	analysis.cartNumber = cartNumber;
+	analysis.category = "initiation";
+	var atn = parseAtnFromGSAAdvantage(str);
+	analysis.attention = atn;
+	consolePrint(analysis);
+	return analysis;
+    } else {
+	var reg = /Re: please approve Cart Number: (\d+)/gm;
+	var myArray = reg.exec(str);
+	if (myArray) {
+	    var arrayLength = myArray.length;
+	    for (var i = 0; i < arrayLength; i++) {
+//		console.log("Replied Cart Number: "+myArray[i]);
+	    }
+	    var cartNumber = myArray[1];
+	    analysis.cartNumber = cartNumber;
+	    analysis.category = "approvalreply";	    
+	    analysis.fromAddress = parseFromEmail(str);
+	    analysis.gsaUsername = parseGsaUsername(str);
+	    analysis.date = parseDate(str);
+	    consolePrint(analysis);
+	    return analysis;
+	}
     }
     return null;
 }
@@ -123,63 +193,78 @@ imap.once('ready', function() {
 		console.log('Message #%d', seqno);
 		var prefix = '(#' + seqno + ') ';
 		msg.on('body', function(stream, info) {
-		    console.log(prefix + 'Body');
 
-		    // Now the plan is to buffer this, and at the end
-		    // (or in the subject!) we can read the cart number via a pattern match.
-		    // The cart number can then be used DIRECTLY in a web service call 
-		    // to our scraping function, which builds up the type, and then 
-		    // we figure out how to send the new email.
-		    // 1) Get Cart #. - 1 Hour
-		    // 2) Invoke a new Web Service to get the email. -- Could take a couple of hours. 
-		    // 3) Send the new Email based on the webservice. -- Could take a couple of hours.
 		    var buffer = '', count = 0;
 		    
 		    stream.on('data', function(chunk) {
-			//			console.log('got %d bytes of data [%s]', chunk.length,chunk);
 			buffer += chunk.toString('utf8');
 		    })
 
 		    stream.on('end', function() {
 			console.log('there will be no more data.');
-			var cartId = parseCartIdFromGSAAdvantage(buffer);
-			var recipientEmail = parseAtnFromGSAAdvantage(buffer);
-      		        console.log('XXXX:' + recipientEmail);
-			if (cartId && recipientEmail) {
-			    var options = {
-				host: 'gsa-advantage-scraper',
-				path: String.format('/cgi-bin/gsa-adv-cart.py?p={0}&u={1}&cart_id={2}',
-						    encodeURIComponent(GSA_PASSWORD),encodeURIComponent(GSA_USERNAME),cartId)
-			    };
+			// We must categorize the email, the most basic 
+			// categorization being a two:
+			// *) An initiation email send from GSA Advantage.
+			// *) A reply.
+			console.log('Total Email:'+buffer);
+			var analysis = analyze_category(buffer);
+			if (!analysis) {
+			    console.log('Cannot categorize, doing nothing!');
+			} else if (analysis.category == "initiation") {
+			    var cartId = this.cartNumber;
+			    var recipientEmail = this.attention;
+      		            console.log('XXXX:' + recipientEmail);
+			    if (cartId && recipientEmail) {
+				var options = {
+				    host: 'gsa-advantage-scraper',
+				    path: String.format('/cgi-bin/gsa-adv-cart.py?p={0}&u={1}&cart_id={2}',
+							encodeURIComponent(GSA_PASSWORD),encodeURIComponent(GSA_USERNAME),cartId)
+				};
 
-			    callback = function(response) {
-				var str = '';
+				callback = function(response) {
+				    var str = '';
 
-				//another chunk of data has been recieved, so append it to `str`
-				response.on('data', function (chunk) {
-				    str += chunk;
-				});
+				    //another chunk of data has been recieved, so append it to `str`
+				    response.on('data', function (chunk) {
+					str += chunk;
+				    });
 
-				//the whole response has been recieved, so we just print it out here
-				response.on('end', function () {
-				    var data = eval(str);
-				    var rendered_html = c2render.renderListCart(c2render.generateCart(data));
-				    var subject = "please approve Cart Number: "+cartId;
-				    var from = GSA_USERNAME;
-				    sendFrDynoCart(from, recipientEmail, subject, rendered_html)
-				});
-			    };
+				    //the whole response has been recieved, so we just print it out here
+				    response.on('end', function () {
+					var data = eval(str);
+					var rendered_html = c2render.renderListCart(c2render.generateCart(data));
+					var subject = "please approve Cart Number: "+cartId;
+					var from = GSA_USERNAME;
+					sendFrDynoCart(from, recipientEmail, subject, rendered_html)
+				    });
+				};
+				http.request(options, callback).end();
+                            };
+			} else if (analysis.category == "approvalreply") {
+			    // Now we must treat this as a reply...
+			    // But in fact I have no endpoint to sent this to.
+			    // Therefore as a stopgap I will send an "approval received" 
+			    // email to the originator...if I can determine it!
+			    // To simulate this, I have created a 
+			    var subject = "Approved for Cart Number: "+analysis.cartNumber;
+                            var emailBody = String.format('At time {0}, approver {1} approved Car Number {2}.  So please fulfill the order with all deliberate speed.',
+							  analysis.date,
+							  analysis.fromAddress,
+							  analysis.cartNumber);
 
-			    http.request(options, callback).end();
-			}
+			    sendFrDynoCart(analysis.fromAddress,
+					   simulatedMapOfUserNameToEmail[analysis.gsaUsername],
+					   subject,
+					   emailBody			    
+					  );
+			};
 		    });
-		    
-		});
-		msg.once('attributes', function(attrs) {
-		    console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
-		});
-		msg.once('end', function() {
-		    console.log(prefix + 'Finished');
+		    msg.once('attributes', function(attrs) {
+			console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+		    });
+		    msg.once('end', function() {
+			console.log(prefix + 'Finished');
+		    });
 		});
 	    });
 	    f.once('error', function(err) {
@@ -194,11 +279,11 @@ imap.once('ready', function() {
 });
 
 imap.once('error', function(err) {
-  console.log(err);
+    console.log(err);
 });
 
 imap.once('end', function() {
-  console.log('Connection ended');
+    console.log('Connection ended');
 });
 
 imap.connect();
