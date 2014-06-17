a8719465b511f9d8656fb2a51f3be150a9d43008
Robert L. Read <read.robert@gmail.com>
2014-06-17 18:42:38 -0400

    not sure where I was here

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,17 @@
+require 'capistrano/ext/multistage'
 require "rvm/capistrano"
 
+set :stages, %w(development staging production)
+set :default_stage, "development"
 
 set :application, "mario"
 set :repository,  "https://github.com/18F/Mario.git"
-set :domain, '54.185.133.124'
+
 set :branch, ENV['BRANCH'] || 'master'
 set :user, "ubuntu"
 set :scm, :git
 set :deploy_to, "/var/www/#{application}"
 set :use_sudo, true
-
-
-role :app, "54.185.133.124"
-
 
 
 desc "Symlink configs"
